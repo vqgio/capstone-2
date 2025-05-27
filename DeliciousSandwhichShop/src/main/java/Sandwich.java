2b0ededbe669303716f636fd95e5324a72afd976
Gio <giovannizavala82@gmail.com>
2025-05-27 11:02:02 -0500

    started sandwich class, have an if statement for no bread or size error.

--- DeliciousSandwhichShop/src/main/java/Sandwich.java
+++ DeliciousSandwhichShop/src/main/java/Sandwich.java
@@ -2,5 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sandwich {
+    private SandwichSize size;
+    private BreadType breadType;
+    private List<Topping> toppings;
+    private boolean isToasted;
 
+    public Sandwich(SandwichSize size, BreadType breadType, List<Topping> toppings, boolean isToasted) {
+        if (size == null || breadType == null) {
+            throw new IllegalArgumentException("Cant have a real sandwich without bread! Start by picking a size and a bread type.");
+        }
+        this.size = size;
+        this.breadType = breadType;
+        this.toppings = new ArrayList<>();
+        this.isToasted = isToasted;
+    }
 }
