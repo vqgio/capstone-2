04c13a824f891a8fa714332924492dfbb67c3b9c
Gio <giovannizavala82@gmail.com>
2025-05-27 22:57:01 -0500

    Got order class working...has a calculate price and a generate receipt class that works...working on creating file saver.

--- DeliciousSandwhichShop/src/main/java/Order.java
+++ DeliciousSandwhichShop/src/main/java/Order.java
@@ -1,6 +1,48 @@
-public class Order {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Order {
+    private String orderNumber;
+    private String customerName;
+    private List<Sandwich> sandwiches;
+    private List<Drink> drinks;
+    private List<Chips> chips;
+
+    private static final String FILE_PATH = "receipts.csv";
+
+    public Order(String orderNumber, String customerName) {
+        this.orderNumber = orderNumber;
+        this.customerName = customerName;
+        this.sandwiches = new ArrayList<>();
+        this.chips =  new ArrayList<>();
+        this.drinks =  new ArrayList<>();
+    }
+    public void addSandwich(Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+    public void addChips(Chips chip) {
+        chips.add(chip);
+    }
     public double calculatePrice() {
-        return 0;
+        double total = 0;
+        total += sandwiches.stream().mapToDouble(Sandwich::calculateCost).sum();
+        total += drinks.stream().mapToDouble(Drink::getPrice).sum();
+        total += chips.stream().mapToDouble(Chips::getPrice).sum();
+        return total;
+    }
+    public String generateReceipt() {
+        StringBuilder receipt = new StringBuilder();
+        receipt.append("Order Number: " + orderNumber + "\n").append("Customer Name: " + customerName + "\n");
+        receipt.append("Sandwiches: \n");
+        sandwiches.forEach(s -> receipt.append("- ").append(s).append("\n"));
+        receipt.append("Drinks: \n");
+        drinks.forEach(d -> receipt.append("- ").append(d).append("\n"));
+        receipt.append("Chips: \n");
+        chips.forEach(c -> receipt.append("- ").append(c).append("\n"));
+        receipt.append("\nTotal Price: $").append(calculatePrice());
+        return receipt.toString();
     }
 }
